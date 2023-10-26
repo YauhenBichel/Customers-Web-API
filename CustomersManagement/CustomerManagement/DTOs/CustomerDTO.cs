12725f1d464b1501e01af02d91357f7b239101db
Yauhen Bichel <ybichel@eisgroup.com>
2023-10-26 17:58:00 +0100

    minor refactoring: emailAdress fieled renaming to emailAddress

--- CustomersManagement/CustomerManagement/DTOs/CustomerDTO.cs
+++ CustomersManagement/CustomerManagement/DTOs/CustomerDTO.cs
@@ -18,7 +18,7 @@ namespace CusomerManagement.DTOs
         public string Surname { get; set; }
         [Required]
         [StringLength(Constants.CUSTOMER_EMAIL_ADDRESS_MAX_LENGTH, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 0)]
-        public string EmailAdress { get; set; }
+        public string EmailAddress { get; set; }
         [Required]
         [StringLength(Constants.CUSTOMER_MOBILE_NUMBER_MAX_LENGTH, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 0)]
         public string MobileNo { get; set; }
--- CustomersManagement/CustomerManagement/Models/Customer.cs
+++ CustomersManagement/CustomerManagement/Models/Customer.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
 using CusomerManagement.Validators;
+using Microsoft.EntityFrameworkCore;
 
 namespace CusomerManagement.Models
 {
-	public class Customer : AuditEntity
+    [Index(nameof(EmailAddress), IsUnique = true)]
+    [Index(nameof(MobileNo), IsUnique = true)]
+    public class Customer : AuditEntity
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Forename { get; set; }
         public string Surname { get; set; }
-        public string EmailAdress { get; set; }
+        public string EmailAddress { get; set; }
         public string MobileNo { get; set; }
         public Boolean InActive { get; set; }
 		public List<Address> Addresses { get; set; }
